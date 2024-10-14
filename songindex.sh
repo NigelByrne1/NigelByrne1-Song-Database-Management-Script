8655f51c7461385bee64a20a02d1939aafff1b89
Nigel <nigelbyrnedesign@gmail.com>
2024-10-14 18:25:54 +0100

    main menu functional

--- songindex.sh
+++ songindex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Author: Nigel Byrne 20058969
-# Music Song Database Management Script
+# Song Database Management Script
 # Description:
-# This script is designed to manage a simple music song database using bash scripting. The database allows users to:
+# This script is designed to manage a simple song database using bash scripting. The database allows users to:
 # 1. Add new song records (including artist name, song title, genre, BPM, and Camelot key).
 # 2. Search for songs by various attributes (artist, title, genre).
 # 3. Remove songs from the database by specifying the title.
@@ -10,3 +10,63 @@
 # 5. Provide a user-friendly menu to interact with these features.
 # The script uses a text file to store the data, ensuring it is lightweight and easy to use for a basic cataloging system.
 
+#clear terminal
+clear
+echo -e "\nSong Database Management System"
+
+#(!-f) searches for file song_database.txt and if there is not result then touch creates one
+if [ ! -f "song_database.txt" ]; then  
+    echo "Database file not found. Creating a new song database file..."
+    touch "song_database.txt"
+fi
+
+#create a variable to hold the filename for textfile
+song_db="song_database.txt"
+
+add_song(){
+    echo "add a new song.."
+    #todo write rest of function
+}
+
+search_song(){
+    echo "search for a song.."
+    #todo write rest of function
+}
+
+remove_song(){
+    echo "remove a song.."
+    #todo write rest of function
+}
+
+generate_report(){
+    echo "generate a report.."
+    #todo write rest of function
+}
+
+main_menu(){
+    while true; do 
+        echo -e "\nMain Menu"
+        echo "1. Add a new song"
+        echo "2. Search for a song"
+        echo "3. Remove a song"
+        echo "4. Generate a report"
+        echo "5. Exit"
+        read -p "Choose an option: " choice
+
+        case $choice in 
+            1) add_song ;;
+            2) search_song ;;
+            3) remove_song ;;
+            4) generate_report ;;
+            5) 
+                echo "Exiting the App..."
+                break
+                ;;
+            *)
+                echo "Invalid option. Please choose a valid menu option"
+                ;;
+        esac
+    done
+}
+
+main_menu
